7619063dd86405b25bef675c4381471a261ba05c
Rkz3399 <2593roushan5@gmail.com>
2025-05-16 13:55:57 +0530

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7262217536:AAHX6ROfNRk1ZH9ZcwokcwvyR4jFtk3fKP8'
-ADMIN_ID = '1065510487'
+API_TOKEN = '8019197971:AAFsZqwTft4KDnrcSEBhj6AsdYa3QCZ9_zs'
+ADMIN_ID = '1141785067'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
